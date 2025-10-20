ac910795ad5ee9b6025de1e20d24f6a5d35ad94f
Maksim <maksim.bragin05@bk.ru>
2025-10-21 01:02:34 +0500

    готовая 4ЛР

--- gulp/tasks/copyImages.js
+++
@@ -1,8 +0,0 @@
-const copyImages = () => {
-  console.log('копируем изображение');
-  return app.gulp.src('src/images/**/*')
-          .pipe(app.gulp.dest('dist/img'))
-          .pipe(app.plugins.browserSync.stream())
-}
-
-export default copyImages
--- src/js/data/navData.js
+++
@@ -1,9 +0,0 @@
-export const navLinks = [
-  { text: 'Главная', href: '#' },
-  { text: 'Что такое GPT?', href: '#' },
-  { text: 'Open AI', href: '#' },
-  { text: 'Кейсы', href: '#' },
-  { text: 'Библиотека', href: '#' }
-];
-
-export default navLinks;
